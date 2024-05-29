81e1ddbc50136fbaa6a678743a44e4634dffee40
clione <diddbstjr5537@naver.com>
2024-05-30 02:08:24 +0900

    채팅 서버 메세지 수신 기능 추가

--- server 양윤석/server.py
+++ server 양윤석/server.py
@@ -3,11 +3,18 @@ import socketserver
 class MyHandler(socketserver.BaseRequestHandler):
     def handle(self):
         print(self.client_address)
+        while True:
+            msg = self.request.recv(1024)
+            if msg.decode() == "/bye":
+                print("exit client")
+                self.request.close()
+                break
+            self.request.send(msg)
 
 class ChatServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-server = ChatServer(("", 8274), MyHandler)
+server = ChatServer(("", 8275), MyHandler)
 server.serve_forever()
 server.shutdown()
 server.server_close()
