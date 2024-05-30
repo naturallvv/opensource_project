d77ad25c6ca9ff5a0d1411de0bb1e6cc1dc1ffe0
clione <diddbstjr5537@naver.com>
2024-05-31 00:33:51 +0900

    찐 최종

--- server 양윤석/server.py
+++ server 양윤석/server.py
@@ -1,41 +1,124 @@
-import socketserver
+import socket
+from threading import Thread
+from PyQt5.QtCore import QObject, pyqtSignal
 
-class MyHandler(socketserver.BaseRequestHandler):
-    users = {}
+import Server.Bot.jedol as jedol
 
-    def handle(self):
-        print(self.client_address)
-        while True:
-            self.request.send("채팅 닉네임을 입력하세요: ".encode())
-            nickname = self.request.recv(1024).decode()
-            if nickname in self.users:
-                self.request.send("이미 등록된 닉네임 입니다.\n".encode())
+class ServerSocket(QObject):
+    update_signal = pyqtSignal(tuple, bool)
+    recv_signal = pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+
+        self.bListen = False
+
+        self.clients = []
+        self.ip = []
+        self.threads = []
+
+        self.update_signal.connect(self.parent.updateClient) # setting
+        self.recv_signal.connect(self.parent.updateMsg) # setting
+
+    def __del__(self):
+        self.stop()
+
+    # Server
+    def start(self, ip, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.server.bind((ip, port))
+        except Exception as e:
+            print('Bind Error :', e)
+            return False
+        else:
+            self.bListen = True
+
+            self.t = Thread(target = self.listen, args = (self.server,))
+            self.t.start()
+            print('Server Listening...')
+
+        return True
+
+    def stop(self):
+        self.bListen = False
+        if hasattr(self, 'server'):
+            self.server.close()
+            print('Server STOP.')
+
+    # Clients
+    def listen(self, server):
+        while self.bListen:
+            server.listen(5)
+            try:
+                client, addr = server.accept()
+            except Exception as e:
+                print('Accept() Error :', e)
+                break
             else:
-                self.users[nickname] = (self.request, self.client_address)
-                print(f"현재 {len(self.users)} 명 참여중..")
+                self.clients.append(client)
+                self.ip.append(addr)
                 
-                for sock, _ in self.users.values():
-                    sock.send(f"{nickname} 님이 입장 했습니다.".encode())
-                break
+                self.update_signal.emit(addr, True)
+
+                t = Thread(target = self.receive, args = (client, addr))
+                self.threads.append(t)
+                t.start()
+
+    def receive(self, client, addr):
         while True:
-            msg = self.request.recv(1024)
-            if msg.decode() == "/bye":
-                print("exit client")
-                self.request.close()
+            try:
+                recv = client.recv(1024)
+            except Exception as e:
+                print('Recv() Error :', e)
                 break
-            for sock, _ in self.users.values():
-                sock.send(f"[{nickname}] {msg.decode()}".encode())
+            else:
+                msg = str(recv, encoding = 'utf-8')
+                if msg:
+                    self.send(msg)
+                    self.recv_signal.emit(msg)
+                    print('[RECV', addr, ']', msg)
 
-        if nickname in self.users:
-            del self.users[nickname]
-            for sock, _ in self.users.values():
-                sock.send(f"{nickname} 님이 퇴장 했습니다.".encode())
-            print("현재 {} 명 참여중".format(len(self.users)))
+                    # Command
+                    jedol.Jedol(self, msg)
 
-class ChatServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
+        self.removeClient(client, addr)
 
-server = ChatServer(("", 8274), MyHandler)
-server.serve_forever()
-server.shutdown()
-server.server_close()
+    def send(self, msg):
+        try:
+            for c in self.clients:
+                c.send(msg.encode())
+        except Exception as e:
+            print('Send() Error :', e)
+
+    def removeClient(self, client, addr):
+        idx = -1
+        for k, v in enumerate(self.clients):
+            if v == client:
+                idx = k
+                break
+
+        client.close()
+        self.clients.remove(client)
+        self.ip.remove(addr)
+        del(self.threads[idx])
+
+        self.update_signal.emit(addr, False)
+        self.resource_Info()
+
+    def removeAllClients(self):
+        for c in self.clients:
+            c.close()
+
+        for addr in self.ip:
+            self.update_signal.emit(addr, False)
+
+        self.clients.clear()
+        self.ip.clear()
+        self.threads.clear()
+
+    def resource_Info(self):
+        print('Number of Client SOCKET\t:', len(self.clients))
+        print('Number of Client IP\t:', len(self.ip))
+        print('Number of Client THREAD\t:', len(self.threads))
