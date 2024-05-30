b125af8b8a8ef5e945d565fc03ecbc4777d73f0c
clione <diddbstjr5537@naver.com>
2024-05-31 00:35:58 +0900

    클라이언트 찐 최종

--- server 양윤석/clinet.py
+++ server 양윤석/clinet.py
@@ -1,22 +1,72 @@
 import socket
 from threading import Thread
+from PyQt5.QtCore import QObject, pyqtSignal
 
-def recv_message(sock):
-    while True:
-        msg = sock.recv(1024)
-        print(msg.decode())
+class Signal(QObject):
+    recv_signal = pyqtSignal(str)
+    disconnect_signal = pyqtSignal()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("127.0.0.1", 8274))
+class ClientSocket:
+    def __init__(self, parent):
+        self.parent = parent
 
-th = Thread(target=recv_message, args=(sock, ))
-th.daemon = True
-th.start()
+        self.bConnect = False
 
-while True:
-    msg = input()
-    sock.send(msg.encode())
-    if msg == "/bye":
-        break
+        self.recv = Signal()
+        self.recv.recv_signal.connect(self.parent.updateMsg) # setting
+
+        self.disconnect = Signal()
+        self.disconnect.disconnect_signal.connect(self.parent.updateDisconnect) # setting
+
+    def __del__(self):
+        self.stop()
+
+    # Client
+    def start(self, ip, port):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client.connect((ip, port))
+        except Exception as e:
+            print('Connect Error :', e)
+            return False
+        else:
+            self.bConnect = True
+            
+            self.t = Thread(target = self.receive, args = (self.client,))
+            self.t.start()
+            print('Connected')
+
+        return True
     
-sock.close()
+    def stop(self):
+        self.bConnect = False
+        if hasattr(self, 'client'):
+            self.client.close()
+            print('Client STOP.')
+
+            del(self.client)
+            self.disconnect.disconnect_signal.emit()
+
+    def receive(self, client):
+        while self.bConnect:
+            try:
+                recv = client.recv(1024)
+            except Exception as e:
+                print('Recv() Error :', e)
+                break
+            else:
+                msg = str(recv, encoding = 'utf-8')
+                if msg:
+                    self.recv.recv_signal.emit(msg)
+                    print('[RECV]', msg)
+
+        self.stop()
+
+    def send(self, name, msg):
+        if not self.bConnect:
+            return
+        try:
+            send_msg = f'[{name}] {msg}'
+            self.client.send(send_msg.encode())
+        except Exception as e:
+            print('Send() Error :', e)
